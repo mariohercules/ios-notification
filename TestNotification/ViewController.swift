c30fc3be5788e4e90f8cd727082d8ae76b99859d
Mario Hercules <mariohercules@hotmail.com>
2018-01-24 23:01:20 -0300

    Initial commit

---
+++ TestNotification/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestNotification
+//
+//  Created by Mario Hercules Portela on 16/12/17.
+//  Copyright © 2017 Mario Hercules Portela. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
